d71d2b21016242ab738f5edc7cc4809ca7dbfa73
Chantal1208 <chan2292@easv365.dk>
2022-01-18 23:19:59 +0100

    boop

--- src/easv/dk/GUI/Controller/Controller.java
+++ src/easv/dk/GUI/Controller/Controller.java
@@ -134,7 +134,16 @@ public class Controller {
     }
 
 
-    public void editCategory(ActionEvent actionEvent) {
+    public void editCategory(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("easv/dk/GUI/View/movieWindow.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.setTitle("Edit Movie");
+        stage.centerOnScreen();
+        stage.show();
     }
 
     public void deleteCategory(ActionEvent actionEvent) throws SQLException {
@@ -147,7 +156,17 @@ public class Controller {
         movieTable.getItems().remove(movieTable.getSelectionModel().getSelectedItem());
     }
 
-    public void editMovies(ActionEvent actionEvent) {
+    public void editMovies(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("easv/dk/GUI/View/movieWindow.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.setTitle("Edit Movie");
+        stage.centerOnScreen();
+        stage.show();
+
     }
 
 
