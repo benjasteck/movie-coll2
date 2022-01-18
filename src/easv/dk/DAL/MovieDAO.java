6792187598f89c1f4043af09a464de64f47c5f67
DaniRonda <dani68h5@easv365.dk>
2022-01-18 12:39:18 +0100

    I want to die rn

--- src/easv/dk/DAL/MovieDAO.java
+++ src/easv/dk/DAL/MovieDAO.java
@@ -10,14 +10,14 @@ import easv.dk.BE.Movie;
 
 public class MovieDAO {
 
-    easv.dk.DAL.ConnectionManager cm;
+    static easv.dk.DAL.ConnectionManager cm;
 
     public MovieDAO() throws IOException {
         cm = new easv.dk.DAL.ConnectionManager();
     }
 
     //Insert values to the movie table.
-    public Movie createMovie(Movie movie) throws Exception {
+    public static Movie createMovie(Movie movie) throws Exception {
         Movie movieCreated = null;
         Connection con = cm.getConnection();
         String sqlSelectMovie = "INSERT INTO MOVIE VALUES(?,?,?,?,?)";
--- src/easv/dk/GUI/Controller/MovieController.java
+++ src/easv/dk/GUI/Controller/MovieController.java
@@ -1,5 +1,7 @@
 package easv.dk.GUI.Controller;
 
+import easv.dk.BE.Movie;
+import easv.dk.DAL.MovieDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -9,15 +11,18 @@ import javafx.scene.control.ComboBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 
 
 
 import javafx.stage.Window;
 import java.awt.*;
 import java.io.File;
+import java.util.Calendar;
 
 public class MovieController {
-
+private Movie movie;
 
     @FXML
     private Button btnChooseFile;
@@ -28,17 +33,30 @@ public class MovieController {
     @FXML
     private ComboBox comboBoxCategory;
 
+    @FXML
+    private Button btnSaveMovie;
 
+    @FXML
+    private TextField txt_title;
 
-   @FXML
+    @FXML
+    private TextField txt_ImbdRating;
+
+    @FXML
+    private TextField txt_userRating;
+
+    @FXML
+    private TextField txt_movieUrl;
+
+    @FXML
     public void cancelEditingMovie(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Discard changes ?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
         alert.showAndWait();
 
 
         if (alert.getResult() == ButtonType.YES) {
-           Stage stage = (Stage) btnCancelEditingMovie.getScene().getWindow();
-           stage.close();
+            Stage stage = (Stage) btnCancelEditingMovie.getScene().getWindow();
+            stage.close();
 
 
         }
@@ -62,10 +80,37 @@ public class MovieController {
         // double-clicking on the "choose" button will open another window with your computer´s files
         Window window = ((Node) (actionEvent.getSource())).getScene().getWindow();
         FileChooser fileChooser = new FileChooser();
-        File file = fileChooser.showOpenDialog(window);
-        actionEvent.consume();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Movie Files", "*.mp4", "*.mpeg4"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File file = fileChooser.showOpenDialog(null);
+
+        if (file != null) {
+            txt_movieUrl.setText(file.getPath());
+
+        } else  {
+            System.out.println("error"); // or something else
+        }
+
     }
 
+
+    @FXML
+    private void saveNewMovie(ActionEvent actionEvent) throws Exception {
+        String title = txt_title.getText();
+        Double imdbRating = Double.parseDouble(txt_ImbdRating.getText());
+        Double userRating = Double.parseDouble(txt_userRating.getText());
+        String lastView = movie.getLastView();
+        String movieUrl = txt_movieUrl.getText();
+
+        Movie movieCreated = new Movie(title, userRating, imdbRating, lastView, movieUrl, 0);
+        MovieDAO.createMovie(movieCreated);
+        Stage stage = (Stage) btnSaveMovie.getScene().getWindow();
+        stage.close();
+    }
 }
+
+
+
 
 
